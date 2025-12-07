c404a599187045a446fe38fdda99f60a39c7fe11
TauOmicronMu <t.a.goodman.25@gmail.com>
2025-12-07 13:35:39 +0000

    Fix file structure for day 2

---
+++ 2/2/2-2.py
@@ -0,0 +1,12 @@
+import re
+
+ids = []
+with open("../2.txt", 'r') as f:  
+	dat = f.readline().strip().split(',')
+	for rng in dat:
+		bounds = rng.split('-')
+		ids += range(int(bounds[0]), int(bounds[1])+1)
+
+invalid_ids = [s for s in ids if re.fullmatch(r"(.+)\1+", str(s)) is not None]
+
+print(sum(invalid_ids))
