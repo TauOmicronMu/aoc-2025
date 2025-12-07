aafc03ea3bbb89859b6a219194daa0590e54d26e
TauOmicronMu <t.a.goodman.25@gmail.com>
2025-12-07 13:05:04 +0000

    solve part 1

--- 2/1/2-1.py
+++ 2/1/2-1.py
@@ -1,14 +1,12 @@
 import re
 
 ids = []
-with open("../2-ex.txt", 'r') as f:  
+with open("../2.txt", 'r') as f:  
 	dat = f.readline().strip().split(',')
 	for rng in dat:
 		bounds = rng.split('-')
 		ids += range(int(bounds[0]), int(bounds[1])+1)
 
-invalid_ids = [s for s in ids if re.fullmatch(r"(.+)\1+", str(s)) is not None]
-
-print(invalid_ids)
+invalid_ids = [s for s in ids if re.fullmatch(r"(.+)\1", str(s)) is not None]
 
 print(sum(invalid_ids))
