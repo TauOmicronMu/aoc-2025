d757be43417e73273d67d4226701302b2ba68ae1
TauOmicronMu <t.a.goodman.25@gmail.com>
2025-12-07 15:50:25 +0000

    Add solution to 5-1

--- 5/1/5-1.py
+++ 5/1/5-1.py
@@ -5,16 +5,7 @@ with open('../5.txt', 'r') as f:
 	lines = f.readlines() 
 
 split_i = lines.index("\n")
-ranges = [r.strip() for r in lines[:split_i]] 
-ingredients = [i.strip() for i in lines[split_i+1:]]
+ranges = [tuple(map(int, r.strip().split('-'))) for r in lines[:split_i]] 
+ingredients = [int(i.strip()) for i in lines[split_i+1:]]
 
-print(len(ingredients))
-
-valid_ids = [] 
-for r in ranges: 
-	bounds = [int(b) for b in r.split('-')]
-	valid_ids += list(range(bounds[0], bounds[1]+1))
-
-print(len(valid_ids))
-
-print(len([i for i in map(int, ingredients) if i in valid_ids]))
+print(len(list(set([i for i in ingredients for r in ranges if i >= r[0] and i <= r[1]]))))
