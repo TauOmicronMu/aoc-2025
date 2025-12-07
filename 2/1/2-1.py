53d066cf291223f747164c1dc905d85120addce1
TauOmicronMu <t.a.goodman.25@gmail.com>
2025-12-07 13:03:41 +0000

    probably solve part 2...

--- 2/1/2-1.py
+++ 2/1/2-1.py
@@ -5,19 +5,10 @@ with open("../2-ex.txt", 'r') as f:
 	dat = f.readline().strip().split(',')
 	for rng in dat:
 		bounds = rng.split('-')
-		ids += range(int(bounds[0]), int(bounds[1]))
+		ids += range(int(bounds[0]), int(bounds[1])+1)
 
-invalid_ids = []
-for s in ids:
-	s = str(s)
-	print(f"id: {s}")
-	substrings = [s[i:j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)] 
-	print(f"substrings: {substrings}")
+invalid_ids = [s for s in ids if re.fullmatch(r"(.+)\1+", str(s)) is not None]
 
-	for ss in substrings:
-		if len(re.findall(f"{ss}", s)) > 1:
-			print(f"substring was repeated: {ss}")
-			invalid_ids.append(int(s)) 
-			break 
+print(invalid_ids)
 
 print(sum(invalid_ids))
