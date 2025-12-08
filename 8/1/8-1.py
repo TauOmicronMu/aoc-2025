12c3ede758ea80f950f91b7238ac66511ba917f0
TauOmicronMu <t.a.goodman.25@gmail.com>
2025-12-08 08:47:00 +0000

    Add solution for 8-1

--- 8/1/8-1.py
+++ 8/1/8-1.py
@@ -7,13 +7,13 @@ from functools import reduce
 import matplotlib.pyplot as plt
 import networkx as nx
 
-TOTAL_CONNS = 10
+TOTAL_CONNS = 1000
 
 def sld(a, b): 
 	return math.sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2 + (b[2]-a[2])**2)
 
 coords = [] 
-with open('../8-ex.txt', 'r') as f:
+with open('../8.txt', 'r') as f:
 	coords = [list(map(int, l.strip().split(','))) for l in f.readlines()]
 
 distances = sorted([(sld(a, b), tuple(a), tuple(b)) for (a, b) in itertools.combinations(coords,2)])
@@ -34,11 +34,10 @@ while num_selected < TOTAL_CONNS:
 	if b not in nodes:
 		G.add_node(b)
 		nodes.append(b)
-	if not is_path(G, a, b):
-		G.add_edge(a, b)
-		num_selected += 1
+	G.add_edge(a, b)
+	num_selected += 1
 
-print(reduce(operator.mul, [len(c) for c in list(sorted(nx.connected_components(G)))[:3]]))
+print(reduce(operator.mul, sorted([len(c) for c in nx.connected_components(G)], reverse=True)[:3]))
 
 nx.draw(G)
 plt.show()
